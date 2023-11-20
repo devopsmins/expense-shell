e82c1a9412953fe1dbcb9a0dbd8eb6d5b81ff0ae
minhaj <minhajfathima1023@gmail.com>
2023-11-20 03:51:02 -0500

    basic commands

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,31 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+log_file="/tmp/expense.log"
+color="\e [33m"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
-systemctl enable nginx
+echo -e "${color} installing Nginx"
+dnf install nginx -y>>log_file
+echo $?
 
-systemctl restart nginx
+
+echo -e "${color} coy enpense config file"
+cp expense.conf /etc/nginx/default.d/expense.conf>>log_file
+echo $?
+
+echo -e "${color} clean olg nginx file"
+rm -rf /usr/share/nginx/html/*>>log_file
+echo $?
+
+echo -e "${color} download frontend application code"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip>>log_file
+echo $?
+
+echo -e "${color} extract downloaded application contet"
+cd /usr/share/nginx/html>>log_file
+unzip /tmp/frontend.zip>>log_file
+echo $?
+
+
+echo -e "${color} starting nginx services"
+systemctl enable nginx>>log_file
+systemctl restart nginx>>log_file
+echo $?
