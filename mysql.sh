7d9ffc5aaa88c420e63d86352db57b0e3daba098
minhaj <minhajfathima1023@gmail.com>
2023-11-20 05:02:06 -0500

    basic commands

--- mysql.sh
+++ mysql.sh
@@ -2,23 +2,23 @@ log_file="/tmp/expense.log"
 color="\e[33m"
 
 echo -e "${color} disable Mysql default version \e[0m"
-dnf module disable mysql -y &>>log_file
+dnf module disable mysql -y &>>$log_file
 echo $?
 
 echo -e "${color} copy Mysql repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 echo $?
 
 echo -e "${color} Install myqsl server \e[0m"
-dnf install mysql-community-server -y &>>log_file
+dnf install mysql-community-server -y &>>$log_file
 echo $?
 
 echo -e "${color} start mysql sever \e[0m"
-systemctl enable mysqld &>>log_file
-systemctl start mysqld &>>log_file
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 echo $?
 
 echo -e "${color} set Mysql password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
 echo $?
 
