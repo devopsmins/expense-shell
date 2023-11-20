3ae0cbfb1dcf1482e13acf5a3d3c1da13eba620b
minhaj <minhajfathima1023@gmail.com>
2023-11-20 04:18:49 -0500

    basic commands

--- backend.sh
+++ backend.sh
@@ -1,27 +1,57 @@
+log_file="/tmp/expense.log"
+color="\e[33m"
+
+echo -e "${color} disable NodeJs default version \e[0m"
 dnf module disable nodejs -y
+echo $?
+
+echo -e "${color} enable NodeJs 18 version\e[0m"
 dnf module enable nodejs:18 -y
+echo $?
 
+echo -e "${color} install nodejs \e[0m"
 dnf install nodejs -y
+echo $?
 
+echo -e "${color} coy backend service fie\e[0m"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 
+
+echo -e "${color} add application use\e[0m"
 useradd expense
+echo $?
 
+
+echo -e "${color} create application directory \e[0m"
 mkdir /app
+echo $?
 
+echo -e "${color} download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+echo $?
+
+echo -e "${color} extract application content \e[0m"
 cd /app
 unzip /tmp/backend.zip
+echo $?
 
-cd /app
+echo -e "${color} download NodeJs dependecies\e[0m"
 npm install
+echo $?
 
+echo -e "${color} Install Mysql client to Load schema \e[0m"
 dnf install mysql -y
-mysql -h mysql-dev.devopsmins.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo $?
 
+echo -e "${color} load schema \e[0m"
+mysql -h mysql-dev.devopsmins.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo $?
+
+echo -e "${color} starting backend services\e[0m"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
-
+echo $?
 
 
