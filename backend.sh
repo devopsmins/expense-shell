d2e30a319ef896a0cd5b58654bfd876ab73008a8
minhaj <minhajfathima1023@gmail.com>
2023-11-20 05:12:46 -0500

    basic commands

--- backend.sh
+++ backend.sh
@@ -27,6 +27,11 @@ echo -e "${color} create application directory \e[0m"
 mkdir /app &>>$log_file
 echo $?
 
+
+echo -e "${color} delete old alication content \e[0m"
+rm -rf /app/* &>>$log_file
+echo $?
+
 echo -e "${color} download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 echo $?
